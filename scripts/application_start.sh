696a9d458439154a8a0f4d1aefb23f8b3127dfb7
Rafael Nunes <rafaelnfsq@gmail.com>
2022-12-30 15:06:20 -0300

    fix: fix application start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,8 +11,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-#install node modules
-npm install
-
 #start our node app in the background
-node main.js > app.out.log 2> app.err.log < /dev/null & 
+sudo docker-compose up
