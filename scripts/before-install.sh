7de20ae031c3c0a8bd5a8a8bd78d784cd476e394
Rafael Nunes <rafaelnfsq@gmail.com>
2022-12-30 15:13:31 -0300

    fix: fix the scripts for aws codedeploy

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -5,6 +5,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/node-app"
 if [ -d "$DIR" ]; then
