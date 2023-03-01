02db24107506c55d1e8d9789fea68482181dd9be
Rafael Nunes <rafaelnfsq@gmail.com>
2023-03-01 15:36:53 -0300

    feat: create jwt and local strategies

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({ usernameField: 'email' });
+  }
+
+  async validate(email: string, password: string): Promise<User> {
+    return await this.authService.validateUser(email, password);
+  }
+}
