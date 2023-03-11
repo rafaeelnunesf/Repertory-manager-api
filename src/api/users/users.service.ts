5418f9528bca63d7f6ae58cebd76f4df4cdba570
Rafael Nunes <rafaelnfsq@gmail.com>
2023-03-11 16:29:58 -0300

    fix: change schema prisma

--- src/api/users/users.service.ts
+++ src/api/users/users.service.ts
@@ -59,7 +59,8 @@ export class UsersService {
       data: {
         ...data,
         password: hashedPassword,
-        team: {
+
+        teams: {
           create: [
             {
               main: true,
